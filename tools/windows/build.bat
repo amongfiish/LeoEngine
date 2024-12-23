92a0356bfd9a7e92668cf11f1e16ceb5359697ff
Lucas J <ljgecko1230@gmail.com>
2024-12-23 00:10:36 -0800

    fix windows build script

--- tools/windows/build.bat
+++ tools/windows/build.bat
@@ -1,5 +1,6 @@
 cd ..\..
 mkdir build
+cmake . -B build
 cd build
 "%ProgramFiles%\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" LeoEngine.vcxproj /p:configuration=debug /p:platform=x64
 
