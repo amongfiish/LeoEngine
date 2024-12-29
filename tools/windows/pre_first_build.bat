6e6915a55fc1afe6c5861e35dedcdd427ba53952
Lucas James <ljgecko1230@gmail.com>
2024-12-29 14:54:32 -0800

    update pre build script to update submodules

--- tools/windows/pre_first_build.bat
+++ tools/windows/pre_first_build.bat
@@ -2,7 +2,10 @@
 
 REM THIS ONLY HAS TO BE RUN BEFORE THE FIRST BUILD
 
-Powershell -executionpolicy remotesigned -File ..\..\lib\sdl_image\external\Get-GitModules.ps1
-Powershell -executionpolicy remotesigned -File ..\..\lib\sdl_mixer\external\Get-GitModules.ps1
-Powershell -executionpolicy remotesigned -File ..\..\lib\sdl_ttf\external\Get-GitModules.ps1
+cd ..\..
+git submodule init
+git submodule update
+Powershell -executionpolicy remotesigned -File lib\sdl_image\external\Get-GitModules.ps1
+Powershell -executionpolicy remotesigned -File lib\sdl_mixer\external\Get-GitModules.ps1
+Powershell -executionpolicy remotesigned -File lib\sdl_ttf\external\Get-GitModules.ps1
 
